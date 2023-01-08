4432fc0c57bb449e733a7d5efaeb73d79d2e917e
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-08 10:16:56 +0530

    Practicing Flutter with Angela

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+            image:
+                NetworkImage("https://static.toiimg.com/photo/95175519.cms")),
+      ),
+    )),
+  );
+}
